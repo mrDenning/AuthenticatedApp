1ef27de578d6d2c5d19a309f3358cdfed536394f
David Denning <dave@squiduo.com>
2022-03-15 15:11:43 +0000

    .

--- AuthenticatedApp.Client/CustomAuthorizationMessageHandler.cs
+++ AuthenticatedApp.Client/CustomAuthorizationMessageHandler.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
 {
-    private static string scope = @"https://squiduob2c.onmicrosoft.com/fb1dcc49-e0a3-48e4-93c5-4fc746c9b7bf/access_app";
-    //private static string scope = @;
-  
-
+    
     public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
         NavigationManager navigationManager, IConfiguration conf)
         : base(provider, navigationManager)
